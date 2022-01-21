f3a25ed6562bbc5450f97c8b59ab9b5691ae3ebd
tschwemmer <teresa.schwemmer@stonybrook.edu>
2022-01-21 14:59:50 -0500

    SD analysis begun for all three datasets, added notes to log.

--- Ionocyte/Ionocyte Analysis Script.R
+++ Ionocyte/Ionocyte Analysis Script.R
@@ -18,11 +18,59 @@ all_1dph$Experiment<-factor(all_1dph$Experiment,levels=c("exp1","exp2","exp3","e
 str(all_10mm)
 all_10mm$Experiment<-factor(all_10mm$Experiment,levels=c("exp2","exp3"))
 
+
 #Standard deviation analysis of duplicates - embryo
 hist(all_emb$SDYolkmm,breaks=50)
-abline(v=mean(all_emb$SDYolkmm)+(3*sd(all_emb$SDYolkmm)),col="red",lwd=3)
+abline(v=mean(all_emb$SDYolkmm)+(3*sd(all_emb$SDYolkmm)),col="red",lwd=3) #mean+3SD = 298.5196
 
 hist(all_emb$SDBodymm,breaks=50)
-abline(v=mean(all_emb$SDBodymm)+(3*sd(all_emb$SDBodymm)),col="blue",lwd=3)
+abline(v=mean(all_emb$SDBodymm)+(3*sd(all_emb$SDBodymm)),col="blue",lwd=3) #mean+3SD = 299.1431
 
 #Find the sample numbers of the most extreme ones
+all_emb$Sample[all_emb$SDYolkmm>298] #2016-373, 2016-456, 2016-457, 2016-458, 2016-525, 2016-542, 2016-544
+all_emb$Sample[all_emb$SDBodymm>299] #2016-380, 2016-517
+
+
+#Standard deviation analysis of duplicates - 1dph
+hist(all_1dph$SDFrontmm,breaks=50) #the problems identified in last year's analysis were corrected: 2016-119, 2016-245a, 2016-270. 400 was the SD threshold. 
+hist(all_1dph$SDBackmm,breaks=50) #same, probs seem to be fixed: 2016-127, 2016-319, 2016-323, 2016-558. SD threshold was 190. 
+
+#I think 2017 data was excluded from first analysis - I'll calculate mean+3sd just in case threshold should be adjusted. 
+#If any 2017 samples are above the threshold I will examine them
+mean(all_1dph$SDFrontmm)+(3*sd(all_1dph$SDFrontmm)) #258.388
+mean(all_1dph$SDBackmm)+(3*sd(all_1dph$SDBackmm)) #165.5559
+all_1dph$Sample[all_1dph$SDFrontmm>258] #2017-373
+all_1dph$Sample[all_1dph$SDBackmm>165] #none from 2017
+
+
+#Standard deviation analysis of duplicates - 10mm
+hist(all_10mm$SDFrontmm,breaks=100)
+abline(v=mean(all_10mm$SDFrontmm)+(3*sd(all_10mm$SDFrontmm)),col="magenta",lwd=3) #There are two very extreme ones (2016-105t and 2016-163t) and the rest seem to be below ~200
+#The extreme ones were both identified in the 2021 analysis, suggesting the problems haven't been fixed in this version of the dataset. 
+#I can just compare the histogram with that one
+hist(data1$FrontSD,breaks=100)
+abline(v=mean(data1$FrontSD)+(3*sd(data1$FrontSD)),col="blue",lwd=3) #it is different, there are three above the line and one just below it. 
+
+all_10mm$SDFrontmm[all_10mm$Sample=="2016-391t"]
+data1$FrontSD[data1$Sample=="391t"]
+#105t hasn't been changed
+#163t hasn't been changed
+#362t changed from 411 to 2 SD. Front Density 1 changed from 919.796 to 341.173, but the count and area are the same. Looking at the counting spreadsheet, density was miscalculated the first time. 
+#391t SD changed from 626.8 to 36.3. Front Density 2 changed from 1173.8 to 338.7. The count stayed the same but area increased. It looks like originally an excluded area was accidentally used as the total area but I fixed it in April. 
+#So I just need to revisit 105t and 163t for Front. 
+
+hist(all_10mm$SDBackmm,breaks=100)
+abline(v=mean(all_10mm$SDBackmm)+(3*sd(all_10mm$SDBackmm)),col="orange",lwd=3) #mean+3sd = 79.29
+all_10mm$Sample[all_10mm$SDBackmm>79]
+#all the same except the last one is 390t instead of 370t - was that a typo? 
+data1$Sample[data1$BackSD>0.000079]
+#No, originally 370t and 390t were both greater than that threshold. 
+data1$BackSD[data1$Sample=="390t"]
+#390t is included now but not before because the threshold I used in 2021 was 0.00009 (or 90 if using mm). 
+#370t changed because originally SD was 110.9, now it is 11.65. 
+#Back Density 2 is higher in the original data (483.7), now it is 310.4. Looks like the area and count changed. I edited it in April 2021. 
+#So the ones that need to be checked are 2016-56t, 2016-57t, 2016-75t, 2016-77t, 2016-233t, 2016-307t, and 2016-390t. 
+
+
+
+
