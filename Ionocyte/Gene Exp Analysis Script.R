542ab660012a15f59efc1ec8c0630e5a687bdc17
tschwemmer <teresa.schwemmer@stonybrook.edu>
2022-03-17 17:16:05 -0400

    Updated Gene expression analysis script

--- Ionocyte/Gene Exp Analysis Script.R
+++ Ionocyte/Gene Exp Analysis Script.R
@@ -6,3 +6,9 @@
 
 #Load data for each life stage and save as an environment so I can easily load it again
 ge_data<-read.csv(file.choose(),header=TRUE)
+
+#check the structure and make variables factors when necessary
+str(ge_data)
+ge_data$age<-factor(ge_data$age,levels=c("Y","T"))
+ge_data$Experiment<-factor(ge_data$Experiment,levels=c("exp2","exp3"))
+ge_data$Experiment<-factor(ge_data$gene,levels=c("nka","vha","nbc","nhe1","nhe3","nkcc1","nkcc2"))
