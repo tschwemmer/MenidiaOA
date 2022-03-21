fd07db99a7476d4dd439a57ad30a61f2400acc77
tschwemmer <teresa.schwemmer@stonybrook.edu>
2022-03-21 17:12:56 -0400

    Fixed column names in Gene Expr Data sheet; worked on script for gene expression analysis; saved environment that includes gene expression data

--- Ionocyte/Gene Exp Analysis Script.R
+++ Ionocyte/Gene Exp Analysis Script.R
@@ -11,4 +11,11 @@ ge_data<-read.csv(file.choose(),header=TRUE)
 str(ge_data)
 ge_data$age<-factor(ge_data$age,levels=c("Y","T"))
 ge_data$Experiment<-factor(ge_data$Experiment,levels=c("exp2","exp3"))
-ge_data$Experiment<-factor(ge_data$gene,levels=c("nka","vha","nbc","nhe1","nhe3","nkcc1","nkcc2"))
+ge_data$gene<-factor(ge_data$gene,levels=c("nka","vha","nbc","nhe1","nhe3","nkcc1","nkcc2"))
+ge_data$CO2.level<-factor(ge_data$CO2.level,levels=c("400uatm","2200uatm","4200uatm"))
+ge_data$CO2.level2<-factor(ge_data$CO2.level2,levels=c("C","H","VH"))
+ge_data$Temp.level<-factor(ge_data$Temp.level,levels=c("17C","20C","24C"))
+ge_data$Temp.level2<-factor(ge_data$Temp.level2,levels=c("L","M","H"))
+str(ge_data)
+
+#
