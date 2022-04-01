b5fa743c6d6addca9032353cbb7cad256a97e18f
tschwemmer <teresa.schwemmer@stonybrook.edu>
2022-04-01 16:56:05 -0400

    Updates to gene exp analysis and gill analysis random number generator

--- Ionocyte/Gene Exp Analysis Script.R
+++ Ionocyte/Gene Exp Analysis Script.R
@@ -18,4 +18,41 @@ ge_data$Temp.level<-factor(ge_data$Temp.level,levels=c("17C","20C","24C"))
 ge_data$Temp.level2<-factor(ge_data$Temp.level2,levels=c("L","M","H"))
 str(ge_data)
 
-#
+#subset the data by gene to make models easier to code
+ge_nka<-ge_data[ge_data$gene=="nka",]
+ge_vha<-ge_data[ge_data$gene=="vha",]
+ge_nbc<-ge_data[ge_data$gene=="nbc",]
+ge_nhe1<-ge_data[ge_data$gene=="nhe1",]
+ge_nhe3<-ge_data[ge_data$gene=="nhe3",]
+ge_nkcc1<-ge_data[ge_data$gene=="nkcc1",]
+ge_nkcc2<-ge_data[ge_data$gene=="nkcc2",]
+
+
+#Continuous analysis - linear regression
+lmnka<-lm(expression~CO2*Temp,data=ge_nka)
+summary(lmnka) #temp significant
+
+lmvha<-lm(expression~CO2*Temp,data=ge_vha)
+summary(lmvha) #temp significant
+
+lmnbc<-lm(expression~CO2*Temp,data=ge_nbc)
+summary(lmnbc)
+
+lmnhe1<-lm(expression~CO2*Temp,data=ge_nhe1)
+summary(lmnhe1) #interaction significant
+
+lmnhe3<-lm(expression~CO2*Temp,data=ge_nhe3)
+summary(lmnhe3) #interaction significant
+
+lmnkcc1<-lm(expression~CO2*Temp,data=ge_nkcc1)
+summary(lmnkcc1) #co2 significant
+
+lmnkcc2<-lm(expression~CO2*Temp,data=ge_nkcc2)
+summary(lmnkcc2) #interaction significant
+
+
+#plot means and all data with linear regressions
+
+
+
+
